988833b2e2395cc1486a97714a9497fbd41a6bc9
Yatzz <108313193+YannExecuted@users.noreply.github.com>
2023-01-11 18:21:14 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,15 +20,15 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : Zyko-MD
+${htjava} *💌 Nama* : Yatz-MD
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Private 🥶
-${htjava} *🎨 Umur* : 18 🥶
-${htjava} *🧮 Kelas* : private
-${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP "MODDER WHATSAPP"
-${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Sukabumi
+${htjava} *🎨 Umur* : 15 🥶
+${htjava} *🧮 Kelas* : 10 ESEMKAH
+${htjava} *🧩 Hobby* : MENDENGARKAN MUSIK 
+${htjava} *💬 Sifat* : GATAWU
+${htjava} *🗺️ Tinggal* : Cirebon 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
 ${htjava} *🐈 ɢɪᴛʜᴜʙ:* ${sgh}
 ${htjava} *💌 TY:* ${syt}
