b8c12cf1d662bf009484203c079d58695ad0598a
Yatzz <108313193+YannExecuted@users.noreply.github.com>
2023-01-13 18:15:42 +0700

    Update gktau.js

--- plugins/gktau.js
+++ plugins/gktau.js
@@ -2,11 +2,11 @@
 let { MessageType } = (await import('@adiwajshing/baileys')).default
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
-  m.reply(text + '\n Hai Saya ZykoBotz-MD Jangan Lupa Suscribe Youtube ZykoBotz-MD Official\n\nAtau bisa klik link Youtube : https://youtube.com/@zykobotz ', null, {
+  m.reply(text + '\n Hai Saya AkenoBotz-MD Jangan Lupa Suscribe Youtube AkenoBotz-MD Official\n\nAtau bisa klik link Youtube : https://www.youtube.com/@AkenoBotz ', null, {
     contextInfo: { mentionedJid: users }
   })
 }
-handler.command = ['zyko-md']
+handler.command = ['akeno-md']
 
 handler.admin = false
 handler.group = false
